7fb69068533e4c28f5e152e02ac17d854460c344
unknown <yasenyankov21@gmail.com>
2022-05-15 17:41:51 +0300

    first commit

---
+++ lib/chat_web/live/page_live.ex
@@ -0,0 +1,18 @@
+defmodule ChatWeb.PageLive do
+  use ChatWeb, :live_view
+  require Logger
+
+  @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, query: "", results: %{})}
+  end
+
+  @impl true
+  def handle_event("random-room",_params,socket) do
+    random_slug="/" <> MnemonicSlugs.generate_slug(4)
+    Logger.info(random_slug)
+    {:noreply,push_redirect(socket,to: random_slug)}
+  end
+
+
+end
---
+++ lib/chat_web/live/room_live.ex
@@ -0,0 +1,69 @@
+defmodule ChatWeb.RoomLive do
+  use ChatWeb, :live_view
+  require Logger
+
+  @impl true
+  def mount(%{"id"=> room_id},_session, socket) do
+    topic = "room:" <> room_id
+    username=MnemonicSlugs.generate_slug(2)
+
+    if connected?(socket) do
+      ChatWeb.Endpoint.subscribe(topic)
+      ChatWeb.Presence.track(self(),topic,username,%{})
+    end
+
+    {:ok, assign(socket,
+    room_id: room_id,
+    topic: topic,
+    username: username,
+    message: "",
+    messages: [],
+    user_list: [],
+    temporary_assigns: [messages: []]
+    )}
+  end
+  @impl true
+  def handle_event("submite_message", %{"chat"=>%{"message" => message}}, socket) do
+    message=%{uuid: UUID.uuid4(),content: message,username: socket.assigns.username}
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic,"new-message",message)
+    {:noreply,assign(socket,message: "")}
+  end
+  @impl true
+  def handle_event("form_update",%{"chat"=>%{"message"=>message}},socket) do
+    {:noreply,assign(socket,message: message)}
+  end
+  @impl true
+  def handle_info(%{event: "new-message",payload: message},socket)  do
+    {:noreply,assign(socket,messages: [message])}
+  end
+  @impl true
+  def  handle_info(%{event: "presence_diff",payload: %{joins: joins,leaves: leaves}}, socket)  do
+    join_messages=
+      joins
+      |> Map.keys()
+      |> Enum.map(fn username->
+        %{type: :system ,uuid: UUID.uuid4(), content: "#{username} joined chat!"}
+      end)
+
+    leave_messages=
+      leaves
+      |> Map.keys()
+      |> Enum.map(fn username->
+        %{type: :system ,uuid: UUID.uuid4(), content: "#{username} left chat!"}
+      end)
+
+    user_list=ChatWeb.Presence.list(socket.assigns.topic)|>Map.keys()
+
+    {:noreply,assign(socket,messages: join_messages ++ leave_messages,user_list: user_list)}
+  end
+  def display_message(%{type: :system,uuid: uuid, content: content}) do
+    ~E"""
+      <p id="<%= uuid %>"><em><%= content %></em></p>
+    """
+  end
+  def display_message(%{uuid: uuid, content: content,username: username}) do
+  ~E"""
+     <p id="<%= uuid %>"><%= username %>:<%= content %></p>
+  """
+  end
+end
